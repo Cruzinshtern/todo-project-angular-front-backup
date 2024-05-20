3700bb3885a18f65416f2b2a8035d6cad68c2041
Bohdan <bohdanmatv1@gmail.com>
2024-05-20 19:42:56 +0300

    feature-basic table layout

--- src/app/todos/pages/table/table.component.ts
+++ src/app/todos/pages/table/table.component.ts
@@ -1,11 +1,12 @@
 import { Component, InputSignal, input } from '@angular/core';
 import { ITodo } from '../../../shared/interfaces/todo.interface';
 import { BaseTabViewComponent } from '../../components/base-tab-view/base-tab-view.component';
+import { CustomAngularPaginatorComponent, CustomAngularTableComponent } from 'custom-angular-component-lib';
 
 @Component({
   selector: 'table',
   standalone: true,
-  imports: [],
+  imports: [CustomAngularTableComponent, CustomAngularPaginatorComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
