8f4d349960af2d5c016e7dba06e0d61476dcd494
Bohdan <bohdanmatv1@gmail.com>
2024-06-30 17:01:46 +0300

    envs improved for deployed backend

--- src/environment/environment.dev.ts
+++ src/environment/environment.dev.ts
@@ -1,9 +1,8 @@
-// This file will be replaced by either dev, prod, local files
 export const environment = {
     production: false,
     locales: ['en', 'ru'],
     defaultLocale: 'en',
     project_lang_local_storage_key: 'project_lang',
     project_token: 'project_token',
-    BASIC_URL: 'http://localhost:3000'
+    BASIC_URL: 'https://todo-project-nest.up.railway.app'
   };
--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: true,
     locales: ['en', 'ru'],
     defaultLocale: 'en',
-    project_lang_local_storage_key: 'project_lang'
+    project_lang_local_storage_key: 'project_lang',
+    project_token: 'project_token',
+    BASIC_URL: 'https://todo-project-nest.up.railway.app'
   };
